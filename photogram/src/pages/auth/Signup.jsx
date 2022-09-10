6c47dbf890619710f94677a17ee20758bc640572
Kamesh <mskkamesh4@gmail.com>
2022-09-10 12:49:25 +0530

    router-dom install

---
+++ photogram/src/pages/auth/Signup.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Signup extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className="">
+                
+            </div>
+        );
+    }
+}
+ 
+export default Signup;
