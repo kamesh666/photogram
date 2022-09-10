7e4be225c640ef491ab3fd1c10fc3b16fac36744
Kamesh <mskkamesh4@gmail.com>
2022-09-10 12:38:04 +0530

    adding font in google

--- photogram/tailwind.config.js
+++ photogram/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        cupla:['Mea Culpa','cursive'],
+        secular:["Secular One","sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
